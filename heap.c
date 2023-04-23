ecb7516838bf2bf44347ab31ef670e02791dad70
Replit user <>
2023-04-23 00:30:16 +0000

    new try

--- heap.c
+++ heap.c
@@ -29,9 +29,22 @@ void* heap_top(Heap* pq){
     return pq->heapArray[0].data;
 }
 
-
-
 void heap_push(Heap* pq, void* data, int priority){
+  if(pq->size == pq->capac){
+    pq->capac = pq->capac * 2 + 1;
+    pq->heapArray = (heapElem *) realloc(pq->capac, sizeof(heapElem));
+  }
+
+  int pos = pq->size;
+
+  while(pq->heapArray[(pos-1)/2].priority < priority){
+    pq->heapArray[pos] = pq->heapArray[(pos-1)/2];
+    pos = (pos-1)/2;
+  }
+
+  pq->heapArray[pos].priority = priority;
+  pq->heapArray[pos].data = data;
+  pq->size++;
 
 }
 
